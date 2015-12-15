5bbf4fb0bec42f2625b81b1336b89aebfb7a423b
Katy Moe <katy@katy.moe>
2015-12-15 19:14:49 +0000

    remove unnecessary var

--- chapter_2/exercise_1.go
+++ chapter_2/exercise_1.go
@@ -6,6 +6,5 @@ func main() {
   fmt.Print("What is your name? ")
   var name string
   fmt.Scanln(&name)
-  var greeting string = "Hello, " + name + ", nice to meet you!"
-  fmt.Println(greeting)
+  fmt.Println("Hello,", name + ", nice to meet you!")
 }
