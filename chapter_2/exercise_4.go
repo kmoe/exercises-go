7b50eed794d120607d295b3f74d1ab403bc009b9
Katy Moe <katy@katy.moe>
2015-12-16 18:49:13 +0000

    mad lib

---
+++ chapter_2/exercise_4.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Print("Enter a noun: ")
+  var noun string
+  fmt.Scanln(&noun)
+  fmt.Print("Enter a verb: ")
+  var verb string
+  fmt.Scanln(&verb)
+  fmt.Print("Enter an adjective: ")
+  var adjective string
+  fmt.Scanln(&adjective)
+  fmt.Print("Enter an adverb: ")
+  var adverb string
+  fmt.Scanln(&adverb)
+
+  fmt.Println("Do you", verb, "your", adjective, noun, adverb + "? That's hilarious!")
+}
