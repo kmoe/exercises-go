4478c1e8c3bc849ecb275bcc0ec4a4f4ed1a1562
Katy Moe <katy@katy.moe>
2015-12-19 15:08:57 +0000

    standardise Hungarian-ish notation

--- chapter_3/exercise_7.go
+++ chapter_3/exercise_7.go
@@ -7,19 +7,19 @@ func main() {
   const conversionFactor float64 = 0.09290304
 
   fmt.Print("What is the length of the room in feet? ")
-  var length string
-  fmt.Scan(&length)
+  var lengthString string
+  fmt.Scan(&lengthString)
   fmt.Print("What is the width of the room in feet? ")
-  var width string
-  fmt.Scan(&width)
-  fmt.Println("You entered dimensions of", length, "feet and", width, "feet.")
+  var widthString string
+  fmt.Scan(&widthString)
+  fmt.Println("You entered dimensions of", lengthString, "feet and", widthString, "feet.")
 
   fmt.Println("The area is")
 
-  var lengthNumber, _ = strconv.ParseFloat(length, 64)
-  var widthNumber, _ = strconv.ParseFloat(width, 64)
+  var length, _ = strconv.ParseFloat(lengthString, 64)
+  var width, _ = strconv.ParseFloat(widthString, 64)
 
-  var areaInSquareFeet float64 = lengthNumber * widthNumber
+  var areaInSquareFeet float64 = length * width
   var areaInSquareMetres = areaInSquareFeet * conversionFactor
 
   fmt.Println(areaInSquareFeet, "square feet")
